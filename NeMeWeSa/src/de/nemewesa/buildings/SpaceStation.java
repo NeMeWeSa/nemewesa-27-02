f95508887a6324a9eb64f6dbfb2281b7789d8501
ben <nemewesa@gmail.com>
2017-02-28 13:59:27 +0100

    nur space

--- NeMeWeSa/src/de/nemewesa/buildings/SpaceStation.java
+++ NeMeWeSa/src/de/nemewesa/buildings/SpaceStation.java
@@ -2,39 +2,40 @@ package de.nemewesa.buildings;
 
 import java.util.ArrayList;
 
+import de.nemewesa.level.Planet;
 import de.nemewesa.modules.Module;
 
 public class SpaceStation {
 	
-	public ArrayList<String> warehouse;
 	public String name;
-	// Hier kommt spaeter vom Objekt Human
-	public String humanName;
-	public int researchWas;
+
+	public int researchPoint;
+	public boolean avaiable = false;
+	public int bank;
+
 	
-	public SpaceStation(String name){
+	public SpaceStation(String name) {
 		this.name = name;
+		this.researchPoint = 0;
+		this.bank = 0;
+		this.avaiable = false;
 	}
 	
-	public static void researching()
-	{
+	public static void researching() {
 		
 	}
 	
 	// Hier wird das Modul upgegradet
-	public int moduleUpgrate(Module level)
-	{
+	public int moduleUpgrate(Module level) {
 		System.out.println("Das Modul " + level.moduleName + " ist eine Stufe aufgestiegen! ");
 		return level.moduleLevel = level.moduleLevel + 1;
 	}
 	
-	public void produce()
-	{
+	public void produce() {
 		
 	}
 	
-	public void defending()
-	{
+	public void defending() {
 		
 	}
 }
